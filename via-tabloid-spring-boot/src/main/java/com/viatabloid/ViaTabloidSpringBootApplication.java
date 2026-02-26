6d82ebfd5acadebda0efbebdd4bd92deabf3b94d
MrPakaCannyBoiz <pongsatorn5762@gmail.com>
2026-02-26 03:39:39 +0100

    feat: initialize Spring Boot application with Maven wrapper and configuration files

---
+++ via-tabloid-spring-boot/src/main/java/com/viatabloid/ViaTabloidSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.viatabloid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaTabloidSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViaTabloidSpringBootApplication.class, args);
+    }
+
+}
